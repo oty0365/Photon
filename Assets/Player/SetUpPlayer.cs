abe0cf1984c4ead6b7060d0e4fb398621e1c5600
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-09-30 21:39:00 +0900

    wip

--- Assets/Player/SetUpPlayer.cs
+++ Assets/Player/SetUpPlayer.cs
@@ -1,17 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using TMPro;
 using UnityEngine;
 
-public class SetUpPlayer : MonoBehaviour
+public class SetUpPlayer : MonoBehaviourPunCallbacks
 {
     private TextMeshPro _playerNameTag;
-
+    private PhotonView _photonView;
     private void Start()
     {
+        _photonView=GetComponent<PhotonView>();
         _playerNameTag = transform.GetChild(1).GetComponent<TextMeshPro>();
-        _playerNameTag.text = DataPrefs.playername;
+        _photonView.Owner.NickName = DataPrefs.playername;
+        _photonView.RPC("GetName",RpcTarget.All);
+
+
+
     }
-    
+
+    [PunRPC]
+    void GetName()
+    {
+        _playerNameTag.text = _photonView.Owner.NickName;
+    }
 }
